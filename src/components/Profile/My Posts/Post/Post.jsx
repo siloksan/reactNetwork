42a7980b6e73bb7065e4e6527bc97d5adfd1bb3c
Tutel <tutel07@gmail.com>
2022-12-23 09:44:48 +0300

    Create component MyAvatar - separate of the "avatar img".

--- src/components/Profile/My Posts/Post/Post.jsx
+++ src/components/Profile/My Posts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import s from './Post.module.css'
+import MyAvatar from "../../MyAvatar/MyAvatar";
+
 
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img
-                src="https://u.kanobu.ru/articles/pics/7e6dc974-43f4-4ad0-9a55-2465566e9662.jpg"
-                alt="avatar"/>
+            <MyAvatar/>
             {props.message}
             <div>
                 <span>{props.like} like</span>
