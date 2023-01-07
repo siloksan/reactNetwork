99f054d66233de997ffc49a422fd8fb9a3cd72a2
Tutel <tutel07@gmail.com>
2023-01-07 12:32:20 +0300

    #53 Use the class component in the Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,39 +3,43 @@ import styles from './Users.module.css'
 import axios from "axios";
 import userPhoto from "../../asets/images/user.png"
 
-const Users = (props) => {
-
-    let getUsers = () => {if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {props.setUsers(response.data.items)
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
         })
-    }}
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-        props.users.map(u => <div className={styles.user} key={u.id}>
+    }
+
+    render = () => {
+        return <div>
+            {
+                this.props.users.map(u => <div className={styles.user} key={u.id}>
             <span className={styles.photoFollow}>
                 <div>
-                    <img src={ u.photos.small != null ? u.photos.small = '' : userPhoto } alt="avatar" className={styles.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small = '' : userPhoto} alt="avatar"
+                         className={styles.userPhoto}/>
                 </div>
                 <div>
                     {u.followed ?
-                        <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                        <button onClick={() => props.follow(u.id)}>Follow</button>}
+                        <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> :
+                        <button onClick={() => this.props.follow(u.id)}>Follow</button>}
                     </div>
             </span>
-            <span className={styles.aboutUser}>
+                    <span className={styles.aboutUser}>
                 <span>
                     <div>{u.name}</div>
-                    <div>{u.status}</div>
+                    <div>{"u.status"}</div>
                 </span>
                 <span className={styles.location}>
                     <div>{"u.location.city"}</div>
                     <div>{"u.location.country"}</div>
                 </span>
             </span>
-        </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
