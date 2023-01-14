ed2554a2581f9f75dac8a58861f9ce0f74c1a4fa
Tutel <tutel07@gmail.com>
2023-01-14 23:16:07 +0300

    #61 added in Header Login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (<header className={s.header}>
         <img src="https://www.logologo.com/logos/abstract-mollusk-sea-shell-free-logo.jpg" alt=""/>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to='/login'>Login</NavLink>}
+        </div>
     </header>)
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,15 +3,17 @@ import {combineReducers, legacy_createStore as createStore} from "redux";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers ({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
     sideBar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    authUser: authReducer
 })
 
 let store = createStore(reducers);
-// window.store = store;
+window.store = store;
 
 export default store;
