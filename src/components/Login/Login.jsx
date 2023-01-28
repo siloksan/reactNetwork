8155d525ef38f2b815e1050ba3a18e50784d1163
Tutel <tutel07@gmail.com>
2023-01-28 21:31:06 +0300

    #75 I connected redux-form, first form Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+//компонента для формы Авторизации
+const LoginForm = (props) => {
+    return (
+        //метод 'handleSubmit' - вроде как предупреждает что данные собраны и предоставляет их дальше через HOC в контейнерную компоненту!?
+        <form onSubmit={props.handleSubmit}>
+            <div><Field placeholder={'Login'} component={'input'} name={'login'}/></div>
+            <div><Field placeholder={'Password'} component={'input'} name={'password'}/></div>
+            <div><Field component={'input'} type={"checkbox"} name={'rememberMe'}/>remember me</div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+//Контейнерная компонента для ''LoginForm', 'reduxForm' - HOC
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
-const Login = () => {
-    return (<h1>Login</h1>)
+//Общая компонента для формы авторизациии ("LoginForm")
+const Login = (props) => {
+    //
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return (<div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>)
 }
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers ({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
     sideBar: sidebarReducer,
     usersPage: usersReducer,
-    authUser: authReducer
+    authUser: authReducer,
+    form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
